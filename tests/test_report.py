ea078edf4c0408274ea1d2ec133682bb36bc2ccb
Tom Elliott <ipse@paregorios.org>
2024-12-06 14:51:57 -0500

    stub tests for the report module

---
+++ tests/test_report.py
@@ -0,0 +1,12 @@
+#
+# This file is part of pleiades_reporter
+# by Tom Elliott for the Institute for the Study of the Ancient World
+# (c) Copyright 2024 by New York University
+# Licensed under the AGPL-3.0; see LICENSE.txt file.
+#
+
+"""
+Test the report module
+"""
+
+from pleiades_reporter.report import PleiadesReport
