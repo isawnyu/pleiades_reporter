f65760b23cb7167c99fd7a1a92daf15e5925077e
Tom Elliott <ipse@paregorios.org>
2024-12-06 14:54:41 -0500

    stub PleaidesReport class

---
+++ src/pleiades_reporter/report.py
@@ -0,0 +1,26 @@
+#
+# This file is part of pleiades_reporter
+# by Tom Elliott for the Institute for the Study of the Ancient World
+# (c) Copyright 2024 by New York University
+# Licensed under the AGPL-3.0; see LICENSE.txt file.
+#
+
+"""
+Define a standard report object
+"""
+
+
+class PleiadesReport:
+    """
+    Standard report objects
+    Capabilities:
+    - title
+    - summary
+    - plain text version
+    - markdown version
+    - hash tags
+    - associated images and alt text
+    """
+
+    def __init__(self):
+        pass
