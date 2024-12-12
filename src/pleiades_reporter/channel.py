03467020ca885c3d5b161ce791ff9d564c0ba805
Tom Elliott <ipse@paregorios.org>
2024-12-12 05:05:06 -0500

    queued channel base class

--- src/pleiades_reporter/channel.py
+++ src/pleiades_reporter/channel.py
@@ -8,7 +8,42 @@
 """
 Implement the Channel base class for queued dissemination channels
 """
+from collections import deque
 
 
 class Channel:
-    pass
+    def __init__(self):
+        self.queue = deque()
+
+    def clear(self):
+        """Remove all content from the queue"""
+        self.queue.clear()
+
+    def enqueue(self, posts: list, first: bool = False):
+        """Add posts to the dissemination queue"""
+        if not isinstance(posts, list):
+            raise TypeError(
+                f"Expected `posts` parameter to be of type list but got {type(posts)}."
+            )
+        if first:
+            self.queue.extendleft(posts)
+        else:
+            self.queue.extend(posts)
+
+    def post_next(self, count: int = 1):
+        """Post the next `count` items in the queue"""
+        for i in range(0, count):
+            try:
+                post = self.queue.pop()
+            except IndexError:
+                break  # queue is empty
+            else:
+                self._post(post)
+
+    def post_now(self, post):
+        """Post this post immediately."""
+        self._post(post)
+
+    def _post(self, post):
+        """Override this method in subclass to handle specific channel API"""
+        pass
--- tests/test_channel.py
+++ tests/test_channel.py
@@ -10,9 +10,59 @@ Test the channel module
 """
 
 from pleiades_reporter.channel import Channel
+from pytest import raises
 
 
 class TestChannel:
+    @classmethod
+    def setup_class(cls):
+        cls.c = Channel()
+        cls.test_posts = "fee fi fo fum".split()
 
     def test_init(self):
-        c = Channel()
+        assert len(self.c.queue) == 0
+
+    def test_clear(self):
+        self.c.enqueue(self.test_posts)
+        self.c.clear()
+        assert len(self.c.queue) == 0
+
+    def test_enqueue(self):
+        self.c.enqueue(self.test_posts)
+        assert len(self.c.queue) == 4
+        self.c.clear()
+
+    def test_enqueue_more(self):
+        self.c.enqueue(self.test_posts)
+        self.c.enqueue(["foo"])
+        assert len(self.c.queue) == 5
+        self.c.clear()
+
+    def test_enqueue_bad(self):
+        with raises(TypeError):
+            self.c.enqueue("foobar")
+        self.c.clear()
+
+    def test_enqueue_first(self):
+        self.c.enqueue(self.test_posts)
+        self.c.enqueue(["foo"], first=True)
+        assert self.c.queue[0] == "foo"
+        self.c.clear()
+
+    def test_enqueue_last(self):
+        self.c.enqueue(self.test_posts)
+        self.c.enqueue(["foo"], first=False)
+        assert self.c.queue[-1] == "foo"
+        self.c.clear()
+
+    def test_post_next(self):
+        self.c.enqueue(self.test_posts)
+        self.c.post_next()
+        assert len(self.c.queue) == 3
+        self.c.clear()
+
+    def test_post(self):
+        self.c.enqueue(self.test_posts)
+        self.c.post_now("foo")
+        assert len(self.c.queue) == 4
+        self.c.clear()
